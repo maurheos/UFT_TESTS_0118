25df727be80e679872b3bd367822d5f2a96c7217
maurheos <maurheos@gmail.com>
2018-01-12 13:52:41 -0500

    Creación API Test en Git TestFlightAPI 12012017

---
+++ slnVariosEjemplos/TestFlightAPI/TestEntities.cs
@@ -0,0 +1,39 @@
+using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using HP.ST.Fwk.RunTimeFWK.Utilities;
+    using HP.ST.Fwk.RunTimeFWK.BindingFWK;
+    
+    namespace Script
+    {
+    
+    public class TestEntities
+    {
+    public ISTRunTimeContext Context = null;
+    public Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource> dataSourceNameToDataSource = new Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource>();
+    
+    protected HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource GetDataSource(string dataSourceName)
+    {
+    if(!dataSourceNameToDataSource.ContainsKey(dataSourceName))
+    	throw new Exception(("A data source with the specified name does not exist."));
+    return dataSourceNameToDataSource[dataSourceName];
+    }
+    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity10 = null;
+    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity7 = null;
+    public HP.ST.Ext.BasicActivities.StartActivity StartActivity1 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Loop<Loop2Input> Loop2 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity9 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity12 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterCloseActivity DataExporterCloseActivity13 = null;
+    public HP.ST.Ext.BasicActivities.EndActivity EndActivity3 = null;
+    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity8 = null;
+    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity11 = null;
+    public HP.ST.Ext.BasicActivities.RelatedDataFetchActivity RelatedDataFetchActivity15 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Sequence Sequence6 = null;
+    public HP.ST.Ext.BasicActivities.ReplaceStringActivity ReplaceStringActivity4 = null;
+    public HP.ST.Ext.BasicActivities.ConcatenateStringsActivity ConcatenateStringsActivity5 = null;
+    
+    }
+    
+    }
+    
